d96d7c86cf436026ce452bf8c70cdd4be56b1a08
Carlos Oliveira <Oliveira_carlos2017@outlook.com.br>
2019-04-14 11:22:53 -0300

    First commit

---
+++ input.c
@@ -0,0 +1,12 @@
+#include "input.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <conio.h>
+
+char validateinput(){
+    bool x=false;
+
+
+
+    return 'l';
+}
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+int main()
+{
+
+        char input;
+        scanf("%s", &input);
+        printf("AWOWOWOWOOWOWOWO");
+        if(input=='n'||'s'){
+            printf("OWOWOWOWOWOWOWOWOWO");
+            return input;
+        }
+        else{
+            printf("UWUWUWUWUUWU");
+            printf("Input invalido: tente novamente");
+
+
+        }
+    return 0;
+}
